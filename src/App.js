f917c21c8f637a3573894178c5ee6ef59d4fc5b9
Loshavii <larulramesh@gmail.com>
2024-07-29 20:58:54 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Note from "./components/Note";
+import NoteForm from './components/NoteForm';
+import NoteList from './components/NoteList';
+import './App.css';
+
+function App() {
+  return (
+<>
+<Note/>
+</> 
+ );
+}
+
+export default App;
